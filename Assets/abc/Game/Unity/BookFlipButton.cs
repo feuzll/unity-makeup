608d26ebe5f962391412c63591beae210d63c774
personal <private@example.com>
2026-03-23 22:26:25 +0700

    some breathing to book flip buttons

--- Assets/abc/Game/Unity/BookFlipButton.cs
+++ Assets/abc/Game/Unity/BookFlipButton.cs
@@ -1,3 +1,4 @@
+using PrimeTween;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -12,6 +13,18 @@ namespace abc.Game.Unity
         [SerializeField] private SelfRepeatingBookProAdapter _book;
         [SerializeField] private FlipDirection              _direction;
 
+        [Header("Breathing")]
+        [SerializeField] private float _breathScale  = 1.08f;
+        [SerializeField] private float _breathPeriod = 1.6f;
+        
+        private void Start() =>
+            Tween.Scale(transform, startValue: 1f, endValue: _breathScale,
+                new TweenSettings(_breathPeriod / 2f, Ease.InOutSine, cycles: -1,
+                    cycleMode: CycleMode.Yoyo));
+
+        private void OnDestroy() =>
+            Tween.StopAll(transform);
+        
         public void OnPointerClick(PointerEventData _)
         {
             switch (_direction)
